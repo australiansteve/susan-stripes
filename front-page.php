8364fccf4a90884c046e0c746e04cdced3c956d0
australiansteve <steve@australiansteve.com>
2015-05-06 21:02:41 -0300

    Remove sidebar from default front page

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 <div class="row"><!-- .row start -->
 
-	<div class="medium-8 small-12 columns"><!-- .columns start -->
+	<div class="small-12 columns"><!-- .columns start -->
 
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
@@ -41,12 +41,6 @@ get_header(); ?>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
-
-	</div><!-- .columns end -->
-
-	<div class="medium-4 small-12 columns"><!-- .columns start -->
-
-		<?php get_sidebar(); ?>
 
 	</div><!-- .columns end -->
 
