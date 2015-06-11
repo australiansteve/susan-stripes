df85ec553812f3fb7a0e8606e1d319e10a33c747
australiansteve <steve@australiansteve.com>
2015-06-10 22:48:00 -0300

    Remove posts from 'page', display static image only

--- page.php
+++ page.php
@@ -16,20 +16,7 @@ get_header(); ?>
 
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'page-templates/partials/content', 'page' ); ?>
-
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
-
-				<?php endwhile; // end of the loop. ?>
-
+				<img src="http://weavercrawford.com/wp-content/uploads/2015/04/welcome-inv.jpg">
 			</main><!-- #main -->
 		</div><!-- #primary -->
 
