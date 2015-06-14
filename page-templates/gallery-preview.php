ff900e795ba0b94b71aa8b44c8858befeb8c2a17
australiansteve <steve@australiansteve.com>
2015-06-13 21:04:31 -0300

    Add 'gallery preview' template that displays a sidebar as it's only content. Displays the sidebar for austeve_gallery_widget plugin in this instance

---
+++ page-templates/gallery-preview.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Gallery preview
+ * 
+ * @package Susanstripes
+ */
+
+?>
+<!-- austeve-dynamic-content -->
+<div class="small-12 columns"><!-- .columns start -->
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
+
+				<?php dynamic_sidebar('austeve_gallery_1'); ?>
+			
+			</ul>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</div><!-- .columns end -->
+<!-- end austeve-dynamic-content -->
+
