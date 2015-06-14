c3d952542714a0085b98933fd25c9354b430296f
australiansteve <steve@australiansteve.com>
2015-06-13 21:03:22 -0300

    Remove comments sections from existing templates

--- page-templates/content-only.php
+++ page-templates/content-only.php
@@ -2,7 +2,7 @@
 /**
  * Template Name: Content only
  * 
- * @package Woodstove
+ * @package Susanstripes
  */
 
 ?>
@@ -15,13 +15,6 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'page-templates/partials/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
 
 			<?php endwhile; // end of the loop. ?>
 
--- page-templates/full-width.php
+++ page-templates/full-width.php
@@ -2,7 +2,7 @@
 /**
  * Template Name: Full width
  * 
- * @package Woodstove
+ * @package Susanstripes
  */
 
 get_header(); ?>
@@ -15,13 +15,6 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'page-templates/partials/content', 'page' ); ?>
-
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
 
 				<?php endwhile; // end of the loop. ?>
 
